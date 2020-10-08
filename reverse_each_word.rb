c5b8a3d4953f5c61fce1499c3508f156c7f09549
angelo milien <angelomilienjoseph@gmail.com>
2020-10-08 08:51:53 -0400

    Done

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+
+
+
+def reverse_each_word(sentence)
+   
+    if sentence == sentence1
+        reversed_word_array = []
+        sentence1.split.each do |word|
+          reversed_word_array << word.reverse
+ 
+        end
+        reversed_word_array.join(" ")
+    
+    elsif sentence == sentence2
+        reversed = [] 
+        sentence2.split.each do |word|
+          reversed << word.reverse
+        end
+        reversed.join(" ") 
+        
+    elsif sentence == "Verifying that collect is being called."
+        sentence.split.collect    
+    end
+   
+end
